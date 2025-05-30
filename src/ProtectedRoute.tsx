d912d7c01ab5a60740e34ff7e8334db6edb92306
Rishikesh <rushi0241@gmail.com>
2025-05-31 00:57:04 +0530

    delete unwanted files

--- src/ProtectedRoute.tsx
+++ src/ProtectedRoute.tsx
@@ -3,7 +3,7 @@ import { Navigate } from "react-router-dom";
 const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
   const isAuthenticated = !!localStorage.getItem("token");
 
-  return isAuthenticated ? <>{children}</> : <Navigate to="/login" replace />;
+  return isAuthenticated ? <>{children}</> : <Navigate to="/" replace />;
 };
 
 export default ProtectedRoute;
